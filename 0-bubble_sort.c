f3e02f1f644333fa737df5510cdd490d385d9d2f
Edith Mokeira <edithmokeira7@gmail.com>
2024-02-16 22:27:58 +0300

    Added: Task 0 and Task 1 modified files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble Sort Algorithm.
+ *
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,12 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion Sort algorithm
+ * @list: A pointer to the head of the doubly linked list
+ *
+ */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
