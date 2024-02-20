5c56b26659ec49586c1f73be1ecac225f76274b8
Fromb23 <rombo.programming@gmail.com>
2024-02-20 08:10:17 -0800

    modified task 2, new correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,9 +21,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[minInt])
 				minInt = j;
 		}
-		temp = array[minInt];
-		array[minInt] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		if (minInt != i)
+		{
+			temp = array[minInt];
+			array[minInt] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
